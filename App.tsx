cfb57f25978e8322aa1685a58bc8269aa573b3a0
Mikkaiser <mikaelrsimoes19@gmail.com>
2022-06-17 00:06:17 -0300

    add: highlight card component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import { Dashboard } from './src/screens/Dashboard';
+import React from "react";
+import { Dashboard } from "./src/screens/Dashboard";
 
-import { ThemeProvider } from 'styled-components';
-import theme from './src/global/styles/theme';
+import {
+  useFonts,
+  Poppins_500Medium,
+  Poppins_400Regular,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+import * as SplashScreen from "expo-splash-screen";
+
+import { ThemeProvider } from "styled-components";
+import theme from "./src/global/styles/theme";
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
+  const [fontsLoaded] = useFonts({
+    Poppins_500Medium,
+    Poppins_400Regular,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) return null;
+
+  SplashScreen.hideAsync();
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
     </ThemeProvider>
-
-  )
+  );
 }
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+import HighlightCard from "../../components/HighlightCard";
 
-import { Container, Title } from './styles';
+import {
+  UserInfo,
+  UserWrapper,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from "./styles";
+
+import { Container, Header } from "./styles";
 
 export function Dashboard() {
-    return (
-        <Container>
-            <Title>Dashboard</Title>
-        </Container>
-    )
+  const imageURI = "https://avatars.githubusercontent.com/u/72523657?v=4";
+
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: imageURI }} />
+            <User>
+              <UserGreeting>Olá</UserGreeting>
+              <UserName>Mikkaiser</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCard />
+    </Container>
+  );
 }
